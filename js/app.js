e7d64ded5cbcff0a2492a35ea451cce013db27a4
Nate Green <heynategreen@gmail.com>
2016-04-26 17:19:42 -0400

    Fix: menu stays open when About link is clicked

--- js/app.js
+++ js/app.js
@@ -2,4 +2,7 @@ $(document).ready(function(){
   $('#menu-button').click(function(){
     $('header').toggleClass('open');
   });
+  $('header.open li a').click(function(){
+    $('header').toggleClass('open');
+  });
 });
